b2d6e0f395a4ed789649ae127e3caaa86df17232
Alex Stroev <stroev.alexander@gmail.com>
2023-12-04 23:10:53 +0300

    gsap scrollto added

--- __src/layout/scripts_DEV.js
+++ __src/layout/scripts_DEV.js
@@ -3,9 +3,19 @@
 //     /////////////////////////////////// end ready
 // });
 gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollToPlugin);
 
 import { fixHeader } from "../components/header/fixHeader";
 fixHeader(".js-mainheader");
+
+const anchorLinks = document.querySelectorAll(".header__menu a");
+anchorLinks.forEach((item) => item.addEventListener("click", scroll2anchor));
+
+function scroll2anchor(e) {
+    e.preventDefault();
+    const aim = e.target.getAttribute("href");
+    gsap.to(window, { duration: 1, scrollTo: { y: aim, offsetY: 60 } });
+}
 
 import { gsapTeamMembers } from "../components/team-members/gsapTeamMembers";
 gsapTeamMembers();

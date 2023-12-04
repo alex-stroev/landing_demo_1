1541bd5437b4b2a37c37b805ad4fb324a94073b5
Alex Stroev <stroev.alexander@gmail.com>
2023-12-04 22:35:57 +0300

    + fixheader

--- __src/components/header/fixHeader.js
+++ __src/components/header/fixHeader.js
@@ -1,28 +1,67 @@
-function fixHeader(headerElem, fixinClass = "i-fixed", additionalPadding = 20) {
+function fixHeader(
+    headerClass,
+    fixinClassOnDesktop = "i-fixed",
+    openClassForMobile = "i-open",
+    hamburgerClass = ".js-hamburger",
+    additionalPadding = 20,
+    breakpoint = 1024
+) {
     // TODO: учесть возможные верхний паддинг сиблинга и возможность нахождения перед хедером других блоков
 
-    console.log(headerElem);
-    let header = document.querySelectorAll(headerElem);
+    console.log(headerClass);
+    let header = document.querySelectorAll(headerClass);
+    let hamburger = document.querySelectorAll(hamburgerClass)[0];
+
     if (header.length) {
         header = header[0];
+        breakpoint = parseInt(breakpoint);
 
         const afterHeader = header.nextElementSibling;
-        // const afterHeaderPaddingTop = afterHeader.style;
 
-        const toggleFixedClass = () => {
-            console.log(header.offsetTop);
+        hamburger.addEventListener("click", (e) => {
+            e.currentTarget.classList.toggle("is-active");
+            header.classList.toggle("i-open");
+        });
+
+        toggleFixedClass(afterHeader); // for load
+        window.addEventListener("scroll", () => toggleFixedClass(afterHeader));
+
+        ////////////////////////////////////////////////////////////////////////////
+        //  ONRESIZE
+        ////////////////////////////////////////////////////////////////////////////
+        window.addEventListener("resize", () => {
+            if (window.matchMedia(`(max-width: ${breakpoint}px)`).matches) {
+                ////////////////////////////////////////////////////////////////////////////
+                //  MOBILE
+                ////////////////////////////////////////////////////////////////////////////
+                // console.log("MOBILE");
+                header.classList.remove(fixinClassOnDesktop);
+            } else {
+                ////////////////////////////////////////////////////////////////////////////
+                //  DESKTOP
+                ////////////////////////////////////////////////////////////////////////////
+                // console.log("DESKTOP");
+                header.classList.remove(openClassForMobile);
+                hamburger.classList.remove("is-active");
+
+                // const afterHeaderPaddingTop = afterHeader.style;
+            }
+
+            // end resize
+        });
+    }
+
+    function toggleFixedClass(afterHeader) {
+        if (!window.matchMedia(`(max-width: ${breakpoint}px)`).matches) {
             if (window.scrollY > header.offsetHeight) {
-                header.classList.add(fixinClass);
+                header.classList.add(fixinClassOnDesktop);
                 afterHeader.style.paddingTop =
                     header.offsetHeight + additionalPadding + "px";
             } else {
-                header.classList.remove(fixinClass);
+                header.classList.remove(fixinClassOnDesktop);
                 afterHeader.style.paddingTop = "";
             }
-        };
-
-        toggleFixedClass(); // for load
-        window.addEventListener("scroll", toggleFixedClass);
+        }
     }
 }
 
--- __src/layout/scripts_DEV.js
+++ __src/layout/scripts_DEV.js
@@ -5,14 +5,7 @@
 gsap.registerPlugin(ScrollTrigger);
 
 import { fixHeader } from "../components/header/fixHeader";
-// fixHeader(".js-mainheader");
-
-let header = document.querySelector(".js-mainheader");
-let hamburger = document.querySelector(".js-hamburger");
-hamburger.addEventListener("click", (e) => {
-    e.currentTarget.classList.toggle("is-active");
-    header.classList.toggle("i-open");
-});
+fixHeader(".js-mainheader");
 
 import { gsapTeamMembers } from "../components/team-members/gsapTeamMembers";
 gsapTeamMembers();

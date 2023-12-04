0dceaf5d6b42cec3b777d4e0c1b70c4514d76892
Alex Stroev <stroev.alexander@gmail.com>
2023-12-04 21:12:54 +0300

    ".header" + mobile + hamburgers

--- __src/layout/scripts_DEV.js
+++ __src/layout/scripts_DEV.js
@@ -7,5 +7,12 @@ gsap.registerPlugin(ScrollTrigger);
 import { fixHeader } from "../components/header/fixHeader";
 // fixHeader(".js-mainheader");
 
+let header = document.querySelector(".js-mainheader");
+let hamburger = document.querySelector(".js-hamburger");
+hamburger.addEventListener("click", (e) => {
+    e.currentTarget.classList.toggle("is-active");
+    header.classList.toggle("i-open");
+});
+
 import { gsapTeamMembers } from "../components/team-members/gsapTeamMembers";
 gsapTeamMembers();

8fb42aa172245a36b39b04eec982b51de0aa736c
Alex Stroev <stroev.alexander@gmail.com>
2023-12-07 01:28:14 +0300

    whoweare animation fixed

--- __src/components/whoweare/gsapWhoweare.js
+++ __src/components/whoweare/gsapWhoweare.js
@@ -1,27 +1,41 @@
 const gsapWhoweare = () => {
     const tlDuration = 0.25;
-    // if (window.matchMedia("(max-width: 400px)").matches) {
-    //     //
-    // } else {
-    // }
 
     let tl = gsap.timeline({ scrollTrigger: { trigger: ".whoweare__kv" } });
+
     const from = { x: "100%", autoAlpha: 0 };
     const to = { x: 0, autoAlpha: 1, duration: tlDuration };
 
-    tl.fromTo(".whoweare__preheader", from, { ...to, delay: 1 });
-    tl.fromTo(".whoweare__header", from, to);
-    tl.fromTo(".whoweare__text", from, to);
-    tl.fromTo(
+    const toggleWidth = "(max-width: 1024px)";
+    const textDelay = window.matchMedia(toggleWidth).matches ? 0 : 1;
+
+    let tlText = gsap.timeline();
+    tlText.fromTo(".whoweare__preheader", from, { ...to, delay: textDelay });
+    tlText.fromTo(".whoweare__header", from, to);
+    tlText.fromTo(".whoweare__text", from, to);
+
+    let tlPics = gsap.timeline();
+    tlPics.fromTo(
         ".whoweare-kv__man",
         { y: "-100%", autoAlpha: 0 },
         { y: 0, autoAlpha: 1, duration: tlDuration * 2, ease: "bounce.out" }
     );
-    tl.fromTo(
+    tlPics.fromTo(
         ".whoweare-kv__badge",
         { y: "100%", autoAlpha: 0 },
-        { y: "15.3%", autoAlpha: 1, duration: tlDuration * 2, ease: "bounce.out" }
+        {
+            y: "15.3%",
+            autoAlpha: 1,
+            duration: tlDuration * 2,
+            ease: "bounce.out",
+        }
     );
+
+    if (window.matchMedia(toggleWidth).matches) {
+        tl.add(tlPics).add(tlText);
+    } else {
+        tl.add(tlText).add(tlPics);
+    }
 };
 
 export { gsapWhoweare };
